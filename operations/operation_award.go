d2f61b817476543f9f4dd69ae336e127e5ea792c
Vladimir Babin <vovababin@gmail.com>
2021-01-14 20:32:35 +0500

    Change beneficiaries type for AwardOperation

--- operations/operation_award.go
+++ operations/operation_award.go
@@ -1,20 +1,18 @@
 package operations
 
 import (
-	"encoding/json"
-
 	"github.com/VIZ-Blockchain/viz-go-lib/encoding/transaction"
 	"github.com/VIZ-Blockchain/viz-go-lib/types"
 )
 
 //AwardOperation represents award operation data.
 type AwardOperation struct {
-	Initiator      string        `json:"initiator"`
-	Receiver       string        `json:"receiver"`
-	Energy         uint16        `json:"energy"`
-	CustomSequence uint64        `json:"custom_sequence"`
-	Memo           string        `json:"memo"`
-	Beneficiaries  []interface{} `json:"beneficiaries"`
+	Initiator      string              `json:"initiator"`
+	Receiver       string              `json:"receiver"`
+	Energy         uint16              `json:"energy"`
+	CustomSequence uint64              `json:"custom_sequence"`
+	Memo           string              `json:"memo"`
+	Beneficiaries  []types.Beneficiary `json:"beneficiaries"`
 }
 
 //Type function that defines the type of operation AwardOperation.
@@ -37,16 +35,11 @@ func (op *AwardOperation) MarshalTransaction(encoder *transaction.Encoder) error
 	enc.Encode(op.CustomSequence)
 	enc.Encode(op.Memo)
 	if len(op.Beneficiaries) > 0 {
-		//Parse Beneficiaries
-		z, _ := json.Marshal(op.Beneficiaries[0])
-		var l []interface{}
-		_ = json.Unmarshal(z, &l)
-		z1, _ := json.Marshal(l[1])
-		var d types.CommentPayoutBeneficiaries
-		_ = json.Unmarshal(z1, &d)
-
-		enc.Encode(byte(1))
-		enc.Encode(d)
+		enc.EncodeUVarint(uint64(len(op.Beneficiaries)))
+		for _, val := range op.Beneficiaries {
+			enc.Encode(val.Account)
+			enc.Encode(val.Weight)
+		}
 	} else {
 		enc.Encode(byte(0))
 	}
