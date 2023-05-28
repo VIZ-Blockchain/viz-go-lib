01e69280976686e73d08818fb8662f1667641170
biter777 <33280398+biter777@users.noreply.github.com>
2023-05-28 12:03:17 +0300

    Add files via upload

---
+++ transports/websocket/go.mod
@@ -0,0 +1,3 @@
+module websocket
+
+go 1.19
--- transports/websocket/transport.go
+++ transports/websocket/transport.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/VIZ-Blockchain/viz-go-lib/types"
+	"github.com/biter777/viz-go-lib/types"
 	"github.com/gorilla/websocket"
 )
 
